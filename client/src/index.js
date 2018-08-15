6a77dab0d8202461697b8ffed9fc13aad7bf6cb0
Dagestan05 <steklofan@gmail.com>
2018-08-15 04:28:06 +0200

    initial setup or redux & 1st component

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import { createStore, applyMiddleware, compose } from "redux";
+import { Provider } from "react-redux";
+import promiseMiddleware from "redux-promise";
+import thunk from "redux-thunk";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import Routes from "./Routes";
+import rootReducer from "./reducers";
+
+const storeWithMiddleware = createStore(
+  rootReducer,
+  compose(
+    applyMiddleware(promiseMiddleware, thunk),
+    window.devToolsExtension ? window.devToolsExtension() : f => f
+  )
+);
+
+ReactDOM.render(
+  <Provider store={storeWithMiddleware}>
+    <BrowserRouter>
+      <Routes />
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById("root")
+);
